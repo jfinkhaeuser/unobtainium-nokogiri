725c58c064e3c3d8f0443a9ff8007fff398dfd0e
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-04-27 16:52:35 +0200

    Add implementation, specs and test data

--- lib/unobtainium-nokogiri/driver.rb
+++ lib/unobtainium-nokogiri/driver.rb
@@ -16,19 +16,118 @@ module Unobtainium
     # TODO
     class Driver
       class << self
+        ##
+        # Return true if the given label matches this driver implementation,
+        # false otherwise.
         def matches?(label)
           return :nokogiri == label.to_sym
         end
 
+        ##
+        # Ensure that the driver's preconditions are fulfilled.
         def ensure_preconditions(_, _)
-          # FIXME: better error
           require 'nokogiri'
+          require 'open-uri'
+        rescue LoadError => err
+          raise LoadError, "#{err.message}: you need to add "\
+                "'nokogiri' to your Gemfile to use this driver!",
+                err.backtrace
         end
 
+        ##
+        # Create and return a driver instance
         def create(_, _)
-          # TODO
+          driver = ::Unobtainium::Nokogiri::Driver.new
+          return driver
         end
       end # class << self
+
+      ##
+      # "Go to" the given URI, i.e. open it and retain contents.
+      def goto(uri)
+        reset
+
+        # Fetch content
+        open(uri) do |f|
+          @meta[:uri] = uri.dup
+          if f.respond_to?(:metas)
+            @meta[:headers] = f.metas.dup
+            @meta[:status] = f.status.dup
+            @meta[:base_uri] = f.base_uri.dup
+          end
+
+          @content = f.read
+        end
+
+        # Pass content to Nokigiri
+        @parsed = ::Nokogiri.send(@parse_method, @content) do |config|
+          config.options = @options
+        end
+      end
+
+      ##
+      # @return (String) The raw page content, or nil
+      attr_reader :content
+
+      ##
+      # @return (Hash) Any page metadata, or an empty Hash
+      attr_reader :meta
+
+      ##
+      # @return (::Nokogiri::XML::ParseOptions) parser options for all subsequent
+      #   requests.
+      attr_accessor :options
+
+      ##
+      # Set one of :XML or :HTML, deciding which parsing method nokogiri is to
+      # use.
+      def parse_method=(method)
+        if not [:XML, :HTML].include?(method)
+          raise ArgumentError, "parse_method must be one of :XML, :HTML!"
+        end
+        @parse_method = method
+      end
+
+      ##
+      # @return (Symbol) one of :HTML, :XML
+      attr_reader :parse_method
+
+      ##
+      # Map any missing method to nokogiri
+      def respond_to?(meth)
+        if not @parsed.nil? and @parsed.respond_to?(meth)
+          return true
+        end
+        return super
+      end
+
+      ##
+      # Map any missing method to nokogiri
+      def method_missing(meth, *args, &block)
+        if not @parsed.nil? and @parsed.respond_to?(meth)
+          return @parsed.send(meth.to_s, *args, &block)
+        end
+        return super
+      end
+
+      private
+
+      ##
+      # Private initialize to force use of Driver#create.
+      def initialize
+        reset
+
+        @parse_method = :HTML
+        @options = ::Nokogiri::XML::ParseOptions::DEFAULT_HTML
+      end
+
+      ##
+      # Clear all data
+      def reset
+        @content = nil
+        @meta = {}
+        @parsed = nil
+      end
     end # class Driver
 
   end # module Nokogiri
--- spec/driver_spec.rb
+++ spec/driver_spec.rb
@@ -14,4 +14,63 @@ describe 'Unobtainium::Nokogiri::Driver' do
       require_relative '../lib/unobtainium-nokogiri/driver'
     end.to_not raise_error(LoadError)
   end
+
+  it "can be created" do
+    expect do
+      ::Unobtainium::Driver.create(:nokogiri)
+    end.to_not raise_error
+
+    drv = ::Unobtainium::Driver.create(:nokogiri)
+    expect(drv).to_not be_nil
+  end
+
+  it "opens a file" do
+    drv = ::Unobtainium::Driver.create(:nokogiri)
+
+    test_uri = "spec/data/foo.html"
+    expect { drv.goto(test_uri) }.to_not raise_error
+    expect(drv.meta[:uri]).to eql test_uri
+  end
+
+  it "opens a web page" do
+    drv = ::Unobtainium::Driver.create(:nokogiri)
+
+    test_uri = "http://finkhaeuser.de"
+    expect { drv.goto(test_uri) }.to_not raise_error
+    expect(drv.meta[:uri]).to eql test_uri
+
+    expect(drv.meta[:headers]).not_to be_nil
+    expect(drv.meta[:headers]).not_to be_empty
+  end
+
+  it "delegates to nokogiri" do
+    drv = ::Unobtainium::Driver.create(:nokogiri)
+
+    test_uri = "spec/data/foo.html"
+    expect { drv.goto(test_uri) }.to_not raise_error
+
+    expect(drv.xpath('//foo')).not_to be_nil
+    expect(drv.xpath('//foo')).not_to be_empty
+  end
+
+  it "rejects the wrong parse method" do
+    drv = ::Unobtainium::Driver.create(:nokogiri)
+
+    expect { drv.parse_method = :HTML }.not_to raise_error(ArgumentError)
+    expect { drv.parse_method = :XML }.not_to raise_error(ArgumentError)
+
+    expect { drv.parse_method = :foo }.to raise_error(ArgumentError)
+    expect { drv.parse_method = 'XML' }.to raise_error(ArgumentError)
+  end
+
+  it "accepts and interprets configuration" do
+    drv = ::Unobtainium::Driver.create(:nokogiri)
+
+    drv.parse_method = :XML
+    drv.options = ::Nokogiri::XML::ParseOptions::STRICT | \
+                  ::Nokogiri::XML::ParseOptions::PEDANTIC
+
+    test_uri = "spec/data/foo.html"
+    expect { drv.goto(test_uri) }.to raise_error(Nokogiri::XML::SyntaxError)
+  end
 end
