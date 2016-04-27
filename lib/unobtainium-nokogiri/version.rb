f580c4018c55ae3765321d5aa8bc78982d571c20
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-04-27 11:44:45 +0200

    Add version file for unobtainium-nokogiri

---
+++ lib/unobtainium-nokogiri/version.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+#
+# unobtainium-nokogiri
+# https://github.com/jfinkhaeuser/unobtainium-nokogiri
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other unobtainium-nokogiri contributors.
+# All rights reserved.
+#
+module Unobtainium
+module Nokogiri
+  # The current release version
+  VERSION = "0.1.0".freeze
+end # module Nokogiri
+end # module Unobtainium
