bad57e188560d5d2cc9f229d457af7214a416004
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-04-09 13:34:42 +0200

    Bump patchlevel

--- lib/unobtainium-nokogiri/version.rb
+++ lib/unobtainium-nokogiri/version.rb
@@ -9,6 +9,6 @@
 module Unobtainium
   module Nokogiri
     # The current release version
-    VERSION = "0.2.0".freeze
+    VERSION = "0.2.1".freeze
   end # module Nokogiri
 end # module Unobtainium
